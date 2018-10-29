3cd7b84e13f1fa2f903d1c178598bcd48ba8524b
michael <michaelpanik92+shipt@gmail.com>
2018-10-29 13:34:17 -0500

    Controller changes.

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -10,5 +10,24 @@ export default class Controller {
             ['submarine', 3],
             ['destroyer', 2],
         ])
+
+        this.clickHandler()
+    }
+
+    clickHandler() {
+        const tiles = document.querySelectorAll('div.tile')
+
+        tiles.forEach(e => {
+            e.addEventListener('click', e => {
+                const board = [...e.target.parentElement.parentElement.children].indexOf(e.target.parentElement)
+                const tile = [...e.target.parentElement.children].indexOf(e.target)+1
+
+                const y = Math.ceil(tile / 10)
+                const x = tile - ((y-1)*10)
+                
+                const attackEvent = new CustomEvent('attack', { detail: { target: e.target, board, x, y } })
+                document.dispatchEvent(attackEvent)
+            })
+        })
     }
 }
