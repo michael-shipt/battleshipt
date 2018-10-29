2b44b4fdbf93d2870f9bf997fc3c219e268d9582
michael <michaelpanik92+shipt@gmail.com>
2018-10-29 13:34:08 -0500

    View changes.

--- src/View/view.js
+++ src/View/view.js
@@ -4,11 +4,52 @@ export default class View {
     }
 
     drawBoard(board) {
-        const tiles = board.tiles.map(e => {
-            return e.map(() => {
-                return `<div class="tile"></div>`
+        if (!this.boardExists(board.name)) {
+            this.app.innerHTML += `<div class="board" id="${board.name}"></div>`
+        }
+
+        const tiles = board.tiles.map(row => {
+            return row.map(tile => {
+                const className = typeof tile === 'object' ? 'tile--ship' : ''
+                return `<div class="tile ${className}"></div>`
             }).join('')
         }).join('')
-        this.app.innerHTML += `<div class="board">${tiles}</div>`
+        
+        document.getElementById(board.name).innerHTML = tiles
+    }
+
+    boardExists(boardId) {
+        return document.getElementById(boardId) !== null ? true : false
+    }
+
+    setActiveBoard(boardIndex) {
+        const boards = document.querySelectorAll('.board')
+
+        boards.forEach(e => {
+            e.classList.remove('board--active')
+        })
+
+        boards[boardIndex].classList.add('board--active')
+    }
+
+    setTileStatus(status, tile) {
+        switch (status) {
+            case 'hit':
+            case 'sunk':
+            case 'gameover':
+                tile.classList.add('tile--hit')
+                break;
+            
+            case 'miss':
+                tile.classList.add('tile--miss')
+                break;
+            
+            case 'ship':
+                tile.classList.add('tile--ship')
+                break;
+        
+            default:
+                break;
+        }
     }
 }
